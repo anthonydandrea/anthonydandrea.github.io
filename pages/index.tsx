3f003deacec5804a841048c30a0478a9684dd32b
Anthony D'Andrea <anthonydandrea2@gmail.com>
2023-03-02 23:31:39 -0500

    test 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Change! Get started by editing&nbsp;
+            Another change! Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
